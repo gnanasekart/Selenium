872644cadf4fefea9bcdad0d468e1f2a09673ebc
gnanasekart <gnanasekar69@hotmail.com>
2020-12-04 21:39:26 +0530

    update

---
+++ src/main/java/PracticesDemo/Test1.java
@@ -0,0 +1,17 @@
+package PracticesDemo;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Test1 {
+
+	@Test
+	public void testing()
+	{
+		System.out.println("Hi");
+		//Reporter.log("Hi message");
+		
+		System.out.println("end ");
+		
+	}
+}
